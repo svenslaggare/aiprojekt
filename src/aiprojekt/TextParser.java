8616edb55cbc27d83e13632114c0fcb6933f59cc
Andreas Nilsson <anil9@kth.se>
2015-09-27 08:40:33 +0200

    succeeds a realistic testcase. Improved regex.

--- src/aiprojekt/TextParser.java
+++ src/aiprojekt/TextParser.java
@@ -1,17 +1,30 @@
 package aiprojekt;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TextParser {
 
-	private static final String REPLACE_ALL_PATTERN = "\\.";
+	//private static final String REPLACE_ALL_PATTERN = "^\\[[\\d]+:[\\d]+\\] <[\\w]+> |\\.|,|:|;|=";
+	// matches the form [19:32]
+	private static final String MATCH_TIMESTAMP = "^\\[[\\d]+:[\\d]+\\]";
+	// matches the form <svenslaggare>
+	private static final String MATCH_USERNAME = "<[\\w]+>";
+	private static final String MATCH_TIME_AND_USER = MATCH_TIMESTAMP+" "+MATCH_USERNAME+" ";
+	// matches everything but letters a-z - ' (whitespace)
+	private static final String MATCH_UNWANTED_CHARS = "[^a-zA-Z\\-\\' ]";
+	private static final String REPLACE_ALL_PATTERN = MATCH_TIME_AND_USER+"|"+MATCH_UNWANTED_CHARS;
+	//private static final String REPLACE_ALL_PATTERN = "^\\[[\\d]+:[\\d]+\\] <[\\w]+> |[^a-zA-Z\\-\\' ]";
 	
 	public List<String> tokenize(String text) {
 		text = text.replaceAll(REPLACE_ALL_PATTERN, "");
 		text = text.toLowerCase();
 		String[] tokens = text.split("\\s+");
-		return Arrays.asList(tokens);
+		List<String> list = Arrays.asList(tokens);
+		//list.removeAll(Collections.singleton(""));
+		return list;
 		
 	}
 	
--- src/aiprojekt/TextParserTest.java
+++ src/aiprojekt/TextParserTest.java
@@ -9,13 +9,20 @@ import java.util.List;
 import org.junit.Test;
 
 public class TextParserTest {
-	String simpleSentence1 = "Let's try this thing.";
+	String sentence1 = "Let's try this thing.";
 	List<String> answer1 = new LinkedList<String>(Arrays.asList("let's", "try", "this", "thing"));
-
+	
+	String sentence2 = "[21:56] <HedgeMage> Deoovo: I haven't used thin clients much, I'm sorry.  I can answer general questions, though.";
+	List<String> answer2 = new LinkedList<String>(Arrays.asList("deoovo", "i", "haven't", "used", "thin", "clients", "much", "i'm", "sorry", "i", "can", "answer", "general", "questions", "though"));
+	
 	@Test
 	public void testTokenize() {
 		TextParser parser = new TextParser();
-		assertTrue(answer1.equals(parser.tokenize(simpleSentence1)));
+		System.out.println(Arrays.toString(parser.tokenize(sentence1).toArray()));
+		assertTrue(answer1.equals(parser.tokenize(sentence1)));
+		
+		System.out.println(Arrays.toString(parser.tokenize(sentence2).toArray()));
+		assertTrue(answer2.equals(parser.tokenize(sentence2)));
 	}
 
 }
