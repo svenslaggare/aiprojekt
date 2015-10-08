86803488cb1ebef76948c81bd3d75a1eb23a660d
Andreas Nilsson <anil9@kth.se>
2015-10-08 22:13:04 +0200

    A first version of a working evaluator

--- src/aiprojekt/Evaluator.java
+++ src/aiprojekt/Evaluator.java
@@ -7,56 +7,129 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 1. Train model. Use training data to train the NGramModel.
+ * 
+ * 2. Extract testing sentences from test data != training data
+ * 
+ * 3. Test if actual next word == any of the top K predicted words.
+ * 
+ * 4. Store data (the position of the match in the top K list, the word position
+ * in the sentence matched)
+ * 
+ * 5. Print data, ready for evaluation.
+ * 
+ * @author Andreas
+ * 
+ */
 public class Evaluator {
 
 	private NGramModel model;
 	private static final String EVALUATE_FILE = "res/evaluation/evaluate.txt";
 	private static final int NUM_RESULTS = 10;
+	private static final int MAX_SENTENCE_LENGTH = 30;
+	private int[] countTopResultHit = new int[NUM_RESULTS];
+	private int[] countWordPositionHit = new int[MAX_SENTENCE_LENGTH];
+	private int testedWords = 0;
+
 	public static void main(String[] args) {
-		// this is not for user-learning, another method will be used for that case.
+		// this is not for user-learning,
+		// another method will be used for that case.
 		Evaluator evaluator = new Evaluator();
 		evaluator.evaluate();
-		
-		
-		
 
 	}
 
-	private void evaluate() {
+	/**
+	 * Evaluates the NGram model.
+	 */
+	public void evaluate() {
 		// train model
 		model = trainModel();
 		WordPredictor predictor = new WordPredictor(model, NUM_RESULTS);
 
 		// extract testing sentences from test data != training data
 		File file = new File(EVALUATE_FILE);
-		ArrayList<ArrayList<Token>> sentences = processFiles(file); 
-		for(ArrayList<Token> sentence : sentences){
+		ArrayList<ArrayList<Token>> sentences = processFiles(file);
+
+		for (ArrayList<Token> sentence : sentences) {
 			List<Token> sentenceBuilder = new ArrayList<Token>();
-			List<String> predictedWords = new ArrayList<String>();
-			for(Token token : sentence){
+			// Always have to look at the n+1 word
+			int correctWordPosition = 1;
+			for (Token token : sentence) {
+				// we are done with this sentence if reaching the constant max
+				// sentence length
+				// or if reaching the limit size of this sentence* (* = subtract
+				// 1 from sentence, because of </s> tag).
+				if ((correctWordPosition == MAX_SENTENCE_LENGTH + 1)
+						|| (correctWordPosition == sentence.size() - 1)) {
+					break;
+				}
+
 				sentenceBuilder.add(token);
-				predictedWords = predictor.predictNextWord(sentenceBuilder);
-				// test if actual next word == any predicted word.0
+				ArrayList<String> predictedWords = (ArrayList<String>) predictor
+						.predictNextWord(sentenceBuilder, false);
+				predictionCorrectness(predictedWords, sentence,
+						correctWordPosition);
+				correctWordPosition++;
 			}
-			
-			
+
 		}
-		// for every test sentence:
-		//		predict next coming word
-		//		check top 10 predictions
-		//		If predicted, remember which position in the top 10 list
-		//		Otherwise, increase counter of not predicted
-		
-		
+
+		processResults();
+
+	}
+
+	private void predictionCorrectness(ArrayList<String> predictedWords,
+			ArrayList<Token> sentence, int correctWordPosition) {
+		testedWords++;
+		String correctWord = sentence.get(correctWordPosition).toString();
+		//System.out.println("correct word: " + correctWord);
+		for (int i = 0; i < predictedWords.size(); i++) {
+		//	System.out.println("Predicted: " + predictedWords.get(i));
+			if (predictedWords.get(i).equals(correctWord)) {
+				// increment count for match at top-list position i.
+				countTopResultHit[i]++;
+				// increment count for match at word position.
+				countWordPositionHit[correctWordPosition]++;
+			}
+		}
+
 	}
 
 	private NGramModel trainModel() {
 		PreProcessor processor = new PreProcessor();
 		processor.run();
 		return processor.getNgramModel();
-		
+
 	}
-	
+
+	private void processResults() {
+		int totalCorrectPredicted = 0;
+		System.out.println("Count of hits at specified position in top "
+				+ NUM_RESULTS + " predictions:");
+		for (int i = 0; i < NUM_RESULTS; i++) {
+			System.out.println("position " + (i + 1) + ": "
+					+ countTopResultHit[i]);
+			totalCorrectPredicted += countTopResultHit[i];
+		}
+
+		System.out
+				.println("\nCount of hits at specified position in the sentences:");
+		for (int i = 0; i < MAX_SENTENCE_LENGTH; i++) {
+			System.out.println("word " + i + " in the sentence: "
+					+ countWordPositionHit[i]);
+		}
+
+		System.out.println("Total tested words: " + testedWords);
+		System.out.println("Total successfully predicted words: "
+				+ totalCorrectPredicted);
+		System.out
+				.println("Hitrate (totalCorrectPredicted/(double)testedWords): "
+						+ totalCorrectPredicted / (double) testedWords);
+
+	}
+
 	/**
 	 * Tokenizes and indexes the file @code{file}. If @code{file} is a
 	 * directory, all its files and subdirectories are recursively processed.
@@ -64,7 +137,7 @@ public class Evaluator {
 	private ArrayList<ArrayList<Token>> processFiles(File file) {
 		TextParser parser = new TextParser();
 		ArrayList<ArrayList<Token>> listOfSentences = new ArrayList<ArrayList<Token>>();
-		
+
 		// do not try to tokenize fs that cannot be read
 		if (file.canRead()) {
 			if (file.isDirectory()) {
@@ -76,17 +149,19 @@ public class Evaluator {
 					}
 				}
 			} else {
-				try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+				try (BufferedReader br = new BufferedReader(
+						new FileReader(file))) {
 					String sentence;
 					while ((sentence = br.readLine()) != null) {
-						listOfSentences.add((ArrayList<Token>) parser.tokenize(sentence));
+						listOfSentences.add((ArrayList<Token>) parser
+								.tokenize(sentence));
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-	return listOfSentences;
+		return listOfSentences;
 	}
 
 }
